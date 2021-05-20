44f9e04fc464d6f740bf42abc4fc41b9a262614a
Jay Bhakhar <jaybhakhar@gmail.com>
2021-05-20 18:07:36 +0500

    on tap goes to group route

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(Home());
 }
+List faculty = [
+  'Филологический факультет',
+  'Факультет башкирской и тюркской филологии',
+  'Исторический факультет',
+  'Факультет математики и информационных технологий',
+  'Естественнонаучный факультет',
+  'Факультет педагогики и психологии',
+  'Экономический факультет',
+  'Юридический факультет',
+  'Колледж'
+];
 
-class Home extends StatelessWidget {
+List filoGroup = ['ИНФ11',
+                  'ИНФ31',
+                  'ZИНФ11'
+];
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    List faculty = [
-    'Филологический факультет',
-    'Факультет башкирской и тюркской филологии',
-    'Исторический факультет',
-    'Факультет математики и информационных технологий',
-    'Естественнонаучный факультет',
-    'Факультет педагогики и психологии',
-    'Экономический факультет',
-    'Юридический факультет',
-    'Колледж'
-    ];
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -28,17 +38,27 @@ class Home extends StatelessWidget {
           title: Center(child: Text('Расписание СФ БашГУ')),
         ),
         body: SafeArea(
-          child: ListView(
+          child: Column(
             children: [
-              GestureDetector(
-                onTap: (){
-                  print('vasyuhvljh');
-                },
-                child: Column(
-                  children: List.generate(faculty.length,(index){
-    return Text(faculty[index].toString());})
-                ),
-              )
+              Expanded(
+                child: ListView.builder(
+                    itemCount: faculty.length,
+                    itemBuilder: (BuildContext context, int index){
+                  return ListTile(
+                    title: Center(child: Text('${faculty[index]}'),),
+                    onTap: (){
+                      print(index);
+                      // SHOW A GROUP LIST
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Group(facultyNo: index)
+                        ),
+                      );
+                    },
+                  );
+                }),
+              ),
             ],
           ),
         ),
@@ -47,3 +67,18 @@ class Home extends StatelessWidget {
   }
 }
 
+class Group extends StatelessWidget {
+  Group({this.facultyNo});
+  int facultyNo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text('$facultyNo'),
+      ),
+    );
+  }
+}
